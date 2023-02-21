8e2c24a99a9d271663f2f4afc090047b46da57f4
Grapeview2013 <aigulmus@gmail.com>
2023-02-21 20:59:37 +0600

    partially solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.guessNumber = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.guessNumber = Math.round((this.min + this.max) / 2);
+    return this.guessNumber;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.guessNumber - 1;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guessNumber + 1;
+  }
 }
 
 module.exports = GuessingGame;
